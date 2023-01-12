ade524243a288ecb5c7ec6e51e5713dbfb59715b
Yohannes Aregay <emailtoyohannes@gmail.com>
2023-01-12 15:05:24 +0300

    add customerPhone and extras to bill

--- lib/webirr/bill.rb
+++ lib/webirr/bill.rb
@@ -6,21 +6,25 @@ module Webirr
   class Bill
     attr_accessor :customer_code,
                   :customer_name,
+                  :customer_phone,
                   :amount,
                   :description,
                   :bill_reference,
                   :merchant_id,
-                  :time
+                  :time,
+                  :extras
 
     def as_json(_options = {})
       {
         customerCode: @customer_code,
         customerName: @customer_name,
+        customerPhone: @customer_phone
         amount: @amount,
         description: @description,
         billReference: @bill_reference,
         merchantID: @merchant_id,
-        time: @time
+        time: @time,
+        extras: @extras
       }
     end
 
